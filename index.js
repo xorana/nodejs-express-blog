38417ee0ce28b89f48a5d56367b4aa8565927a77
Jacob <jacob.salway@gmail.com>
2019-09-28 16:11:53 +1000

    index.js to app.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-var port = process.env.NODE_ENV == 'production' ? (process.env.port || 80) : 4000;
-var server = app.listen(port, () => {
-    console.log('Server listening on port ' + port);
-});
