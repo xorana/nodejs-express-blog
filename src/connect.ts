0d7ee19c63a93799ed8b45a725fada3434481b14
Jacob Salway <7677151+xorana@users.noreply.github.com>
2020-07-12 23:06:44 +1000

    Dev blog boilerplate

--- src/connect.ts
+++ src/connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default (db: string) => {
     const connect = () => {
-        mongoose.connect(db)
+        mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             return console.info('Sucessfully connected to ${db}');
         })
