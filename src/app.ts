f656a43c03de31be54bd9092a72b2123194759de
Jacob Salway <7677151+xorana@users.noreply.github.com>
2020-10-25 18:07:07 +1100

    Stability commit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import path from "path";
-
-import connect from './connect';
-import Post from './models/post.model';
+import compression from "compression";
 
 const app = express();
 const port = 4000;
-// const port = process.env.NODE_ENV == "production" ? (process.env.port || 80) : 4000;
+
+app.use(compression());
 
 app.use("/static", express.static(path.join(__dirname, "../public")));
 
@@ -15,15 +14,6 @@ app.set("view engine", "pug");
 
 app.get("/", (req, res) => res.render("index"));
 
-app.get("/blog", (req, res) => {
-    Post.find({}, (err, posts) => {
-        res.render("blog", {posts: [{'title': 'test1', 'time': Date.now(), 'content': ['this is some test content']}]})
-    });
-});
-
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
-
-const db = 'mongodb://localhost:27017';
-connect(db);
