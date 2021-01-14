5d8b33845dbfbd505bd02ece356c0469a27be256
Jacob Salway <jacob.salway@gmail.com>
2021-01-14 21:54:05 +1100

    Update action

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd ~
-pm2 startOrReload npm --name "jacobsalway.com" -- run serve
+pm2 describe jacobsalway.com > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -eq 0 ]; then
+    pm2 restart jacobsalway.com
+else
+    pm2 start npm --name "jacobsalway.com" -- run serve
+fi
